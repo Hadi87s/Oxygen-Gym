d8e73f1d2452c8e011b47f3121cf8e235a6ca360
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2024-12-29 12:20:25 +0200

    Finished the ContactUs and Added a template for Trainers page.

--- Sections/ContactUs/scripts/contact.js
+++ Sections/ContactUs/scripts/contact.js
@@ -1,41 +1,69 @@
-// Map and marker variables
 let map;
 let marker;
 
-// Locations with their coordinates
 const locations = {
-  dubai: { lat: 25.276987, lng: 55.296249 },
-  kuwait: { lat: 29.375859, lng: 47.977405 },
-  abu_dhabi: { lat: 24.453884, lng: 54.3773438 },
-  sabah_alsalem: { lat: 29.2654343, lng: 48.0826247 },
+  abu_dhabi: {
+    lat: 24.4148508,
+    lng: 54.4995307,
+  },
+  dubai: {
+    lat: 25.117138,
+    lng: 55.409291,
+  },
+  jabriya: {
+    lat: 29.3227831,
+    lng: 48.0179689,
+  },
+  sabah_alsaleem: {
+    lat: 29.2654343,
+    lng: 48.0826247,
+  },
+  al_adalia: {
+    lat: 29.3282726,
+    lng: 47.984206,
+  },
+  new_riggae: {
+    lat: 29.3095995,
+    lng: 47.9189881,
+  },
+  al_mahboula: {
+    lat: 29.145836,
+    lng: 48.120453,
+  },
 };
 
-// Initialize the map
 function initMap(lat = 25.276987, lng = 55.296249) {
   const location = { lat, lng };
+
   map = new google.maps.Map(document.getElementById("map"), {
     center: location,
     zoom: 12,
+    mapTypeControl: false,
   });
 
-  // Place a marker
   marker = new google.maps.Marker({
     position: location,
     map: map,
   });
+
+  infoWindow.open(map, marker);
+
+  marker.addListener("click", () => {
+    infoWindow.open(map, marker);
+  });
 }
 
-// Update map when dropdown changes
 document.getElementById("locations").addEventListener("change", (event) => {
   const selectedLocation = event.target.value;
-  const locationCoords = locations[selectedLocation];
+  const locationDetails = locations[selectedLocation];
 
-  if (locationCoords) {
-    // Update map center and marker position
-    map.setCenter(locationCoords);
-    marker.setPosition(locationCoords);
+  if (locationDetails) {
+    const { lat, lng } = locationDetails;
+    map.setCenter({ lat, lng });
+    marker.setPosition({ lat, lng });
+
+    infoWindow.open(map, marker);
   }
 });
 
-// Initialize the map with a default location
 initMap();
