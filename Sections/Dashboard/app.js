205959803bbbc0f8c953281c01c32adc53a7af23
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2024-12-31 22:43:35 +0200

    finishing the interface for adding a new product.

--- Sections/Dashboard/app.js
+++ Sections/Dashboard/app.js
@@ -1,31 +1,42 @@
-const toggleButton = document.getElementById('toggle-btn')
-const sidebar = document.getElementById('sidebar')
+const toggleButton = document.getElementById("toggle-btn");
+const sidebar = document.getElementById("sidebar");
 
-function toggleSidebar(){
-  sidebar.classList.toggle('close')
-  toggleButton.classList.toggle('rotate')
+function toggleSidebar() {
+  sidebar.classList.toggle("close");
+  toggleButton.classList.toggle("rotate");
 
-  closeAllSubMenus()
+  closeAllSubMenus();
 }
 
-function toggleSubMenu(button){
-
-  if(!button.nextElementSibling.classList.contains('show')){
-    closeAllSubMenus()
+function toggleSubMenu(button) {
+  if (!button.nextElementSibling.classList.contains("show")) {
+    closeAllSubMenus();
   }
 
-  button.nextElementSibling.classList.toggle('show')
-  button.classList.toggle('rotate')
+  button.nextElementSibling.classList.toggle("show");
+  button.classList.toggle("rotate");
 
-  if(sidebar.classList.contains('close')){
-    sidebar.classList.toggle('close')
-    toggleButton.classList.toggle('rotate')
+  if (sidebar.classList.contains("close")) {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
   }
 }
 
-function closeAllSubMenus(){
-  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
-    ul.classList.remove('show')
-    ul.previousElementSibling.classList.remove('rotate')
-  })
+function closeAllSubMenus() {
+  Array.from(sidebar.getElementsByClassName("show")).forEach((ul) => {
+    ul.classList.remove("show");
+    ul.previousElementSibling.classList.remove("rotate");
+  });
 }
+
+const addButton = document.getElementById("add-product");
+const addPopUp = document.querySelector(".product-popup");
+const cancelBtn = document.getElementById("cancel-button");
+
+addButton.onclick = () => {
+  addPopUp.style.display = "flex";
+};
+
+cancelBtn.onclick = () => {
+  addPopUp.style.display = "none";
+};
