9c6e2939df84d46aec9c36907f0a196680396a0b
Hadi Irshaid <HadiIrshaid8722@gmail.com>
2025-01-01 14:41:43 +0200

    adding the admin's dashboard login

--- Sections/SignIn/scripts/signIn.js
+++ Sections/SignIn/scripts/signIn.js
@@ -13,3 +13,17 @@ const swiper = new Swiper(".swiper", {
     clickable: true,
   },
 });
+
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const login = document.getElementById("submit-btn");
+
+login.onclick = (e) => {
+  console.log(email.value);
+  console.log(password.value);
+  e.preventDefault(); // Prevents the default form submission behavior
+  if (email.value == "admin@pal.com" && password.value == "admin") {
+    // Redirects to the dashboard
+    window.open("../../../Sections/Dashboard/calendar.html", "_self");
+  }
+};
